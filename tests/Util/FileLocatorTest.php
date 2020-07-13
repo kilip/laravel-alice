caab86d84728b4e6e8134de6a82b143a96895599
Anthonius Munthi <me@itstoni.com>
2020-07-13 14:17:50 +0800

    phpcs

--- tests/Util/FileLocatorTest.php
+++ tests/Util/FileLocatorTest.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Kilip Laravel Alice project.
+ *
+ * (c) Anthonius Munthi <https://itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Tests\Kilip\Laravel\Alice\Util;
 
 use Kilip\Laravel\Alice\Util\FileLocator;
@@ -22,11 +33,11 @@ class FileLocatorTest extends TestCase
         $locator = new FileLocator();
         $locator->addPaths($dir = $this->fixtureDir);
 
-        $files = $locator->find();
+        $files        = $locator->find();
         $expectedFile = $this->fixtureDir.'/1.yml';
 
         $this->assertIsArray($files);
-        $this->assertContains($expectedFile,$files);
+        $this->assertContains($expectedFile, $files);
 
         $locator->addPaths($dir);
         $this->assertCount(2, $files);
